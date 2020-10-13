8831ae75833b4cbe1f64787647e20c19563ed6ce
yaronk <skarlinski@gmail.com>
2020-10-13 14:08:37 +0300

    working as a background file

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-	alert('background');
-	chrome.runtime.sendMessage(tabs[0].id, function(response){
-		console.log(response);
-	});
-})
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+// 	alert('background');
+// 	chrome.runtime.sendMessage(tabs[0].id, function(response){
+// 		console.log(response);
+// 	});
+// })
+chrome.browserAction.onClicked.addListener(function(activeTab) {
 
+    chrome.tabs.executeScript(null, {file: "liker.js"});
+});
--- liker.js
+++ liker.js
@@ -8,14 +8,33 @@ chrome.runtime.onMessage.addListener(
        
        //openPopup();
 function openPopup(){
-	var overlay = document.createElement('div');
-	overlay.innerHTML = fbLikerGetOverlayMarkup();
-	
-	document.body.appendChild(overlay);
+	var popup = document.createElement('div');
+	popup.id = 'entropy-popup';
+	popup.style.position = 'fixed';
+	popup.style.left = '50%';
+	popup.style.top = '50%';
+	popup.style.textAlign = 'center';
+	popup.style.transform = 'translate(-50%, -50%)';
+	popup.style.zIndex = 11;
+	popup.innerHTML = fbLikerGetPopupMarkup();
+	let overlay = document.createElement('div');
+	overlay.id = 'fbLikerOverlay';
+	overlay.onclick = facebookLikerClosePopup;
+	document.body.appendChild(overlay)
+	document.body.appendChild(popup);
 	var submit = document.getElementById('fbLikerSubmit');
 	submit.onclick = fblikerPopupSubmit;
+	[...popup.querySelectorAll('.facebookLikerClosePopup')].map((e) => {
+		e.onclick = facebookLikerClosePopup;
+	})
 	//fblikerPopupSubmit();
  }
+function facebookLikerClosePopup(){
+	let overlay = document.getElementById('fbLikerOverlay');
+	let popup = document.getElementById('entropy-popup')
+	overlay.parentElement.removeChild(overlay)
+	popup.parentElement.removeChild(popup)
+}
 function fblikerPopupSubmit(){
 	alert('liked!');
 	var filters = {"data-testid":'ufi_comment_like_link'};
@@ -37,36 +56,46 @@ function fblikerPopupSubmit(){
 		}
 	}
 }
-
-$.ajax({ 
-    url : chrome.extension.getURL('Assets/EntropyLogos.png'), 
-    processData : false,
-}).always(function(b64data){
-    //$("#fbLikerLogoPlaceHolder").attr("src", "data:image/png;base64,"+b64data);
-    $("#fbLikerLogoPlaceHolder").attr("src", chrome.extension.getURL('Assets/EntropyLogos.png')).fadeIn();
-});
-$('#fbLikerModal').modal({})
-function fbLikerGetOverlayMarkup(){
-	return '<div class="modal fade" id="fbLikerModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
-		  <div class="modal-dialog" role="document">\
-		    <div class="modal-content">\
-		      <div class="modal-header">\
-		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
-		          <span aria-hidden="true">&times;</span>\
-		        </button>\
-		        <h4 class="modal-title" id="myModalLabel">FaceBook Entropy Generator</h4>\
-		      </div>\
-		      <div class="modal-body">\
-		      <img id= "fbLikerLogoPlaceHolder" /> <span>\
-		      Throw a little chaos into your carefully curated persona built on social media. \n\
-		      Get Close to your enemies or alienate your friends. Shed your polished facebook persona by randomly liking a third of all the posts in your news feed\
-		      </span>\
-		      </div>\
-		      <div class="modal-footer">\
-		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
-		        <button id = "fbLikerSubmit" type="button" class="btn btn-primary">Like!</button>\
-		      </div>\
-		    </div>\
-		  </div>\
-		</div>';
+openPopup()
+fetch(chrome.extension.getURL('Assets/EntropyLogos.png')).then(function(res){
+	document.getElementById("fbLikerLogoPlaceHolder").src = chrome.extension.getURL('Assets/EntropyLogos.png')
+})
+// $.ajax({ 
+//     url : 
+//     processData : false,
+// }).always(function(b64data){
+//     //$("#fbLikerLogoPlaceHolder").attr("src", "data:image/png;base64,"+b64data);
+    
+// });
+document.getElementById('fbLikerModal');
+function fbLikerGetPopupMarkup(){
+	return `
+	<div class="" style="posision:absolute;left:50%;top:50%transform: translate(-50%, -50%);" id="fbLikerModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header"  style="text-align:left;background-color:#6e84b5">
+		        <button type="button" onclick="facebookLikerClosePopup" class="close facebookLikerClosePopup" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		        <h4 class="modal-title" style="color:white" id="myModalLabel">Facebook Entropy Generator</h4>
+		      </div>
+		      <div class="modal-body">
+			  <img width="150" id= "fbLikerLogoPlaceHolder" /> <p>
+			  Press the button!<br/>
+			  Get close to your enemies or alienate your friends.
+			  Shed your polished facebook persona by randomly
+			   liking a third of all posts on your wall.
+			  </p>
+			  <p>
+			  <b>ARE YOU BRAVE ENOUGH TO
+			  PRESS THE BUTTON?</b>
+			  </p>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-secondary facebookLikerClosePopup" data-dismiss="modal" >Close</button>
+		        <button id = "fbLikerSubmit" type="button" class="btn btn-primary">Alllow</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>`;
 }
